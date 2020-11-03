89aef99460bee3c8f22cfae35331cdeff356b645
Jerin Lee <taurenjerin@qq.com>
2020-11-03 19:57:19 +0800

    Demo: implement api/v0/account/all; remove api example.
    Signed-off-by: Jerin Lee <taurenjerin@qq.com>

---
+++ src/PayHub/PayHub.Web/Controllers/API/AccountController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace PayHub.Web.Controllers.API {
+  [ApiController]
+  [Route( "api/v0/account" )]
+  public class AccountController : ControllerBase {
+
+    [HttpGet]
+    [Route( "all" )]
+    public IEnumerable<Models.WithdrawAccount> GetAccounts(string username) {
+      // TODO: get user info from cookie/session, authenticate
+
+      // test
+      if (string.IsNullOrEmpty( username ))
+        username = "jerin";
+      var user = Web.Lib.DemoData.TestAuthenticate( username, username, username, username );
+      var currencies = Web.Lib.DemoData.TestCurrencies();
+      var accounts = Web.Lib.DemoData.TestWithdrawAccounts2( user.Account.Id, currencies );
+      return accounts;
+    }
+
+  }
+
+}
